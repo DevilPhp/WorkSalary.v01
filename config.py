74a3492d89bedd99224187c78f422892ea3436b8
DevilPhp <php_devil@yahoo.com>
2026-03-04 17:44:18 +0200

    Main-working-v01.51

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_SERVER', 'localhost')
 DB_PORT = os.getenv('DB_PORT', 5400)
 DB_NAME = os.getenv('DB_NAME', 'knitex')
-DB_SERVER_PORT = os.getenv('DB_SERVER_PORT', 6000)
+DB_SERVER_PORT = os.getenv('DB_SERVER_PORT', 5000)
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 API_SERVER = f"http://{DB_HOST}:{DB_SERVER_PORT}/api"
